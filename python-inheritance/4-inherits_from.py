64f5bdc2004331f687b30b39554688388d9fc88f
Muniira-03 <munirra-alsubaie@gmail.com>
2025-05-28 11:58:01 +0000

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -10,7 +10,4 @@ def inherits_from(obj, a_class):
     """checks if an object is an instance of a class that inherited
        from a class directly or indirectly
     """
-    if isinstance(obj, a_class) is True:
-        return True
-    else:
-        return False
+    retune isinstance(obj, a_class) and type(obj) is not a_class
