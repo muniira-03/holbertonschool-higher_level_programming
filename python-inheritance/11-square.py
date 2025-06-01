1e087b1f065b515df0cea8343a517ea5d3df4c40
muniira-03 <munirra.alsubaie@gmail.com>
2025-06-01 10:48:00 +0000

    task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Write a class Square that inherits from Rectangle"""
+"""
+This module defines a class Square that inherits from Rectangle.
+It includes validation and implements area calculation and string representation.
+"""
 
 
 Rectangle = __import__('9-rectangle').Rectangle
